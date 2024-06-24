3bd4cec3dc99946a6897491b77f45cb78a322896
Paul Franklin <paul.franklin@csuglobal.edu>
2024-06-23 21:00:33 -0400

    proofing phase

--- src/CustomLinkedList.java
+++ src/CustomLinkedList.java
@@ -3,22 +3,46 @@ import java.util.NoSuchElementException;
 
 public class CustomLinkedList {
     private Node head;
-//change here
-    public void insert(int data){
+    public void insert(int data) {
+        Node myNode = new Node(data);
+        if (head == null) {
+            head = myNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = myNode;
+        }
     }
+
     public void delete(int data) {
+        if (head == null) {
+            return;
+        }
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+        System.out.println("This value was not found in the list. No value deleted.");
+        return;
     }
-
-
-    // Other methods...
 
     public Iterator<Integer> iterator() {
         return new LinkedListIterator();
     }
+
     private class Node {
         int data;
         Node next;
-
         Node(int data) {
             this.data = data;
             this.next = null;
@@ -43,8 +67,6 @@ public class CustomLinkedList {
             return data;
         }
     }
-
-    // Other methods...
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,9 @@ import java.util.Iterator;
 public class Main {
     public static void main(String[] args) {
         CustomLinkedList linkedList = new CustomLinkedList();
-
-        // Insert some elementssass
-        linkedList.insert(1);
-        linkedList.insert(2);
-        linkedList.insert(3);
-        // Iterate and display elements
+        linkedList.insert(101);
+        linkedList.insert(24);
+        linkedList.insert(3089);
         Iterator<Integer> iterator = linkedList.iterator();
         while (iterator.hasNext()) {
             System.out.print(iterator.next() + " ");
